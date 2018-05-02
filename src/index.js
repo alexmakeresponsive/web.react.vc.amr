deb33fc61f2a50b9c63fe718c96cb28bdb5f281e
alexandr <connectcreation@yandex.ru>
2018-05-02 15:16:04 +0600

    add redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './index.css';
 
@@ -18,18 +20,64 @@ import {
 } from 'react-router-dom'
 
 
+const initialState = [
+    {
+        link:        '/works/work-1',
+        title:       'Title 1',
+        description: 'Some description 1'
+    },
+    {
+        link:        '/works/work-2',
+        title:       'Title 2',
+        description: 'Some description 2'
+    },
+    {
+        link:        '/works/work-3',
+        title:       'Title 3',
+        description: 'Some description 3'
+    }
+];
+
+
+function works(state=initialState, action) {
+    // console.log(action);
+
+    if (action.type === 'ADD_WORK') {
+        return [
+            ...state,
+            action.work
+        ]
+    }
+
+    return state;
+}
+
+const store = createStore(works);
+
+// console.log(store.getState());
+
+store.subscribe(()=>{
+    console.log('subscribe', store.getState());
+});
+
+// store.dispatch({ type: 'ADD_WORK', work: 'Title of some work 1' });
+// store.dispatch({ type: 'ADD_WORK', work: 'Title of some work 2' });
+
+
 
 
 ReactDOM.render(
-    <BrowserRouter>
-        <div>
-            <App />
-            <Route path='/home' component={Home}/>
-            <Route path='/about' component={About}/>
-            <Route path='/works' component={Works}/>
-            <Route path='/contacts' component={Contacts}/>
-        </div>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <div>
+                <App />
+                <Route path='/home' component={Home}/>
+                <Route path='/about' component={About}/>
+                <Route path='/works' component={Works} works={store}/>
+                <Route path='/contacts' component={Contacts}/>
+            </div>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
 
--- src/routes/Works.js
+++ src/routes/Works.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
+
+// const works = [
+//     {
+//         title:       'Title 1',
+//         description: 'Some description 1'
+//     },
+//     {
+//         title:       'Title 2',
+//         description: 'Some description 2'
+//     }
+// ];
+
 class Works extends Component {
 
     render() {
+        console.log('is it test store?', this.props.testStore);
+
         return (
             <div>
                 <h2>Works</h2>
                 <p>
                     Here you se my works
                 </p>
+                <ul id="worksList">
+                    {this.props.testStore.map((item, index) =>
+                        <div key={index}>
+                            <div>
+                                <a className="link" href={item.link}>{item.title}</a>
+                            </div>
+                            <div>
+                                {item.description}
+                            </div>
+                        </div>
+                    )}
+                </ul>
             </div>
         );
     }
 
 }
 
-export default Works;
+export default connect(
+    state => ({
+        testStore: state
+    }),
+    dispatch => ({})
+)(Works);
